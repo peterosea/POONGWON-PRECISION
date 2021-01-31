e934c478549149a046ca52f3b0643a3835fd0409
Hansanghyeon <999@hyeon.pro>
2021-01-31 16:14:54 +0900

    :sparkles: FNB

---
+++ Theme/template-parts/FNB.php
@@ -0,0 +1,24 @@
+<?php
+$zeplin = get_home_url() . '/wp-content/uploads/zeplin';
+?>
+<footer class="FNB">
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-12 col-xl-8 order-1 order-xl-0">
+        <ul class="info">
+          <li><strong>상호</strong><span>㈜풍원정밀</span></li>
+          <li><strong>주소</strong><span>경기도 안산시 단원구 원시동 740-3 풍원정밀</span></li>
+          <li><strong>대표전화번호</strong><a href="tel:031-493-2107">031-493-2107</a></li>
+        </ul>
+        <p class="copyright">Copyright © 2013 POONGWON. All Rights Reserved. </p>
+      </div>
+      <div class="col-12 col-xl-4 order-0 order-xl-0 pagelistCol">
+        <?php
+        wp_nav_menu(array(
+          'menu' => 'FNB',
+          'container_class' => 'page-list',
+        )); ?>
+      </div>
+    </div>
+  </div>
+</footer>
