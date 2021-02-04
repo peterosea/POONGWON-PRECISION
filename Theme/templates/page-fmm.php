88da2a5ae33004a494aa30bf3e7ec5b52c69e469
999hyeon <999@hyeon.pro>
2021-02-04 14:14:21 +0900

    :sparkles: 사업영역 - FMM (#23)

--- Theme/templates/page-fmm.php
+++ Theme/templates/page-fmm.php
@@ -1,9 +1,10 @@
 <?php
 
 /**
- * Template Name: 금속박
+ * Template Name: FMM
  * Template Post Type: page
  */
+
 
 
 // variables
@@ -14,25 +15,21 @@ $zeplin = get_home_url() . '/wp-content/uploads/zeplin';
 get_header();
 get_template_part('template-parts/header/page');
 ?>
-<main class="pageTemplate metalFoil">
+<main class="pageTemplate fmm">
   <div class="pageTitle">
-    봉지 공정에 사용되는 금속박
-    <div class="subTitle">
-      Encapsulation / Metal Foil
-    </div>
+    고해상도의 디스플레이를 위한 첨단 소재 파인 메탈 마스크
+    <div class="subTitle">FMM, Fine Metal Mask</div>
   </div>
   <div class="section section1">
     <div class="bgCover">
-      <img src="<?php echo $zeplin ?>/1104-0524.jpg" srcset="<?php echo $zeplin ?>/1104-0524@2x.jpg 2x, <?php echo $zeplin ?>/1104-0524@3x.jpg 3x">
+      <img src="<?php echo $zeplin ?>/dam-9914.jpg" srcset="<?php echo $zeplin ?>/dam-9914@2x.jpg 2x, <?php echo $zeplin ?>/dam-9914@3x.jpg 3x">
     </div>
     <div class="container">
       <div class="row">
         <div class="col-4"></div>
         <div class="col-8 contentCol">
-          <p>유기물인 OLED 소재는 수분과 산소에 약하기 때문에
-            투습을 방지하여 수명을 확보하기 위한 박막봉지가
-            반드시 필요합니다.
-            풍원정밀이 생산하는 금속박(Metal Foil)은 유기박막과 무기박막을 3~5겹 정도 감아서 OLED 층을 완전히 덮어 씌우는, 봉지 공정(Encapsulation)의 재료가 됩니다.
+          <p>
+            풍원정밀은 TV 디스플레이의 핵심인 OLED 제조 공정이 아몰레드(AMOLED, 능동형 유기발광다이오드)로 발전됨에 따라 풍원정밀은 아몰레드(AMOLED)용 유기물을 증착하기 위한 바(Bar) 형태의 메탈 마스크를 제작하고 있습니다.
           </p>
         </div>
       </div>
@@ -40,14 +37,17 @@ get_template_part('template-parts/header/page');
   </div>
   <div class="section section2">
     <div class="bgCover">
-      <img src="<?php echo $zeplin ?>/1104-0531.jpg" srcset="<?php echo $zeplin ?>/1104-0531@2x.jpg 2x, <?php echo $zeplin ?>/1104-0531@3x.jpg 3x">
+      <img src="<?php echo $zeplin ?>/dam-9782.jpg" srcset="<?php echo $zeplin ?>/dam-9782@2x.jpg 2x, <?php echo $zeplin ?>/dam-9782@3x.jpg 3x">
     </div>
     <div class="container">
       <div class="row">
         <div class="col-5">
-          <h1>봉지공정은 OLED 증착 공정 후 이어지는 핵심
-            공정으로 매우 중요합니다.</h1>
-          <p>증착을 통해 만들어진 OLED 패널이 외부의 영향을 받지 않고 오랫동안 사용될 수 있도록 마감하는 단계이기 때문입니다. OLED 디스플레이에서 빛을 내는 유기물질과 전극은 산소와 수분에 매우 민감하게 반응해 발광 특성을 잃기 때문에, OLED 디스플레이 제조 공정에서 산소와 수분이 유기물에 침투하지 못하도록 확실하게 밀봉하는 봉지공정에 있어서 풍원 정밀의 금속박(Metal Foil)은 큰 역할을 담당하고 있습니다. </p>
+          <p>
+            TV용 OLED 패널 제작에 사용되는 기판은 크기가 커서 오픈 메탈 마스크로는 작업이 어려워 풍원정밀은 그에 맞는 스틱 바 형태의 메탈 마스크를 독자적으로 개발, 양산하게 되었습니다. 스틱바 마스크는 길이가 길어 인장을 하더라도 쳐지면서 증착대상인 기판에 닿게 되고 이 과정에서 불량을 유발하게 될 수 있습니다.
+          </p>
+          <p class="head">
+            풍원정밀의 스틱바 마스크는 기판과의 간격을 유지하기 위한 돌기 형태의 구조물(지지대)을 고안하여 특허를 받았으며 작업에 있어서 최소한의 오차 범위로 업계에서 뛰어난 기술력을 인정받고 있습니다.
+          </p>
         </div>
         <div class="col-7">
           <img src="<?php echo $zeplin ?>/1104-0531.jpg" style="visibility:hidden">
@@ -56,16 +56,35 @@ get_template_part('template-parts/header/page');
     </div>
   </div>
   <div class="section section3">
+    <div class="bgCover double">
+      <img src="<?php echo $zeplin ?>/dsc-1784.jpg" srcset="<?php echo $zeplin ?>/dsc-1784@2x.jpg 2x, <?php echo $zeplin ?>/dsc-1784@3x.jpg 3x">
+      <img src="<?php echo $zeplin ?>/dam-9832.jpg" srcset="<?php echo $zeplin ?>/dam-9832@2x.jpg 2x, <?php echo $zeplin ?>/dam-9832@3x.jpg 3x">
+    </div>
+    <div class="container">
+      <div class="row">
+        <div class="col-7"></div>
+        <div class="col-5 contentCol">
+          <p>
+            TV용 OLED 패널 제작에 사용되는 기판은 크기가 커서 오픈 메탈 마스크로는 작업이 어려워 풍원정밀은 그에 맞는 스틱 바 형태의 메탈 마스크를 독자적으로 개발, 양산하게 되었습니다. 스틱바 마스크는 길이가 길어 인장을 하더라도 쳐지면서 증착대상인 기판에 닿게 되고 이 과정에서 불량을 유발하게 될 수 있습니다.
+          </p>
+          <p class="head">
+            풍원정밀의 스틱바 마스크는 기판과의 간격을 유지하기 위한 돌기 형태의 구조물(지지대)을 고안하여 특허를 받았으며 작업에 있어서 최소한의 오차 범위로 업계에서 뛰어난 기술력을 인정받고 있습니다.
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="section section4">
     <div class="sliderWrap slickCarouselVideo">
       <?php
-      $metalFoil = get_posts(array(
-        'post_type' => 'metalfoil-slide',
+      $FMM = get_posts(array(
+        'post_type' => 'fmm-slide',
         'post_status' => 'publish'
       ));
       ?>
       <div id="slickSlideBg" class="slickSlideBg">
         <?php
-        foreach ($metalFoil as $p) {
+        foreach ($FMM as $p) {
           $video = get_field('video', $p->ID);
           $videoElement = '';
           if (!empty($video)) {
@@ -106,7 +125,7 @@ HTML;
       </div>
       <div id="slickSlideSmall" class="slickSlideSmall">
         <?php
-        foreach ($metalFoil as $p) {
+        foreach ($FMM as $p) {
           $thumbnailUrl = get_the_post_thumbnail_url($p);
           echo <<<HTML
             <div class="slide">
